e497bc8274ad4ba6f7b9975a9a0e7c551d61c752
joaovcostas <jvc22102003@gmail.com>
2024-01-20 15:35:37 -0300

    create transaction func @ context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -10,7 +10,8 @@ import { ArrowCircleDown, ArrowCircleUp, X } from '@phosphor-icons/react'
 import { z } from 'zod'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { api } from '../../lib/axios'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -22,24 +23,28 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext)
+
   const {
     control,
     register,
     handleSubmit,
     formState: { isSubmitting },
+    reset,
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
     const { description, price, category, type } = data
-    await api.post('/transactions', {
+    await createTransaction({
       description,
       price,
       category,
       type,
-      createdAt: new Date(),
     })
+
+    reset()
   }
 
   return (
